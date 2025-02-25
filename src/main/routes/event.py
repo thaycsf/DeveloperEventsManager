0be3d9c149c3682e7b88a15790a87cd92fccfb6c
Thayna Cristina Da Silva Ferreira <thayna.ferreira@juntossomosmais.com.br>
2025-02-25 19:17:53 -0300

    feat: add controllers

--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -4,14 +4,19 @@ event_route_bp = Blueprint('event_route', __name__)
 
 from src.validators.event_creation_validator import event_creation_validator
 
-from src.http_types.http_response import HttpResponse
 from src.http_types.http_request import HttpRequest
+
+from src.controllers.events.event_creation import EventCreation
+from src.model.repositories.eventos_repository import EventosRepository
 
 @event_route_bp.route('/event', methods=['POST'])
 def create_new_event():
     event_creation_validator(request)
-    #http_request = HttpRequest(body=request.json)
+    http_request = HttpRequest(body=request.json)
     
-    http_response = HttpResponse(body={"message": "Event created"}, status_code=201)
+    eventos_repo = EventosRepository()
+    event_creation = EventCreation(eventos_repo)
+    
+    http_response = event_creation.create(http_request)
     
     return jsonify(http_response.body), http_response.status_code

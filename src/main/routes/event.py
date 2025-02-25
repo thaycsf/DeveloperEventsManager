849acb549bed8e393e81d42c4f971715aeee98aa
Thayna Cristina Da Silva Ferreira <thayna.ferreira@juntossomosmais.com.br>
2025-02-25 01:42:47 -0300

    feat: add http types

--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 event_route_bp = Blueprint('event_route', __name__)
 
+from src.http_types.http_response import HttpResponse
+from src.http_types.http_request import HttpRequest
+
 @event_route_bp.route('/event', methods=['POST'])
 def create_new_event():
-    return jsonify({"message": "Event created"}), 201
+    http_request = HttpRequest(body=request.json)
+    http_response = HttpResponse(body={"message": "Event created"}, status_code=201)
+    return jsonify(http_response.body), http_response.status_code

f457e297c53d4495f41b452a4f080c88a26b4a5d
Thayna Cristina Da Silva Ferreira <thayna.ferreira@juntossomosmais.com.br>
2025-02-25 12:08:58 -0300

    feat: add validators

--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -2,11 +2,16 @@ from flask import Blueprint, jsonify, request
 
 event_route_bp = Blueprint('event_route', __name__)
 
+from src.validators.event_creation_validator import event_creation_validator
+
 from src.http_types.http_response import HttpResponse
 from src.http_types.http_request import HttpRequest
 
 @event_route_bp.route('/event', methods=['POST'])
 def create_new_event():
-    http_request = HttpRequest(body=request.json)
+    event_creation_validator(request)
+    #http_request = HttpRequest(body=request.json)
+    
     http_response = HttpResponse(body={"message": "Event created"}, status_code=201)
+    
     return jsonify(http_response.body), http_response.status_code
--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from src.main.routes.event import event_route_bp
+from src.main.routes.subscriber import subscriber_route_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(event_route_bp)
+app.register_blueprint(subscriber_route_bp)
